1fd6f928f1fce73e2238e72fc48b8e61e33579ab
antek@cruzares.com <antek@cruzares.com>
2011-10-31 22:22:11 +0100

    bBalance is converted to minutes and adding is performed on that value and then again converted to hours and minutes. Added test for FlexAccount.

--- flexAccount.py
+++ flexAccount.py
@@ -20,13 +20,31 @@ class FlexAccount(object):
             self.minutes = initialState.minute
         
     
-    def sumTime(self, timeToSum):
-        self.hours = self.hours - timeToSum.hour
-        self.minutes = self.minutes - timeToSum.minute
+    def sumTime(self, hours, minutes):
         
-        if self.minutes < 0:
-            self.hours = self.hours - 1
-            self.minutes = self.minutes + 60
+        if hours * minutes < 0:
+            raise Exception
+        
+        balanceMinutes = self.hours * 60 + self.minutes
+        minutesToAdd = hours * 60 + minutes
+        balanceMinutes = balanceMinutes + minutesToAdd
+        
+        factor = 1
+        if balanceMinutes < 0:
+            factor = - 1
+        self.hours = balanceMinutes * factor / 60
+        self.minutes = balanceMinutes * factor % 60
+        self.hours *= factor
+        self.minutes *= factor
+#        self.minutes = self.minutes + minutes
+#        
+#        if self.hours 
+#        if self.minutes < 0:
+#            self.hours = self.hours - 1
+#            self.minutes = self.minutes + 60
+#        if self.minutes > 59:
+#            self.hours = self.hours + 1
+#            self.minutes = self.minutes - 60
             
         
         
---
+++ flexAccountTest.py
@@ -0,0 +1,43 @@
+'''
+Created on 31/10/2011
+
+@author: Antek
+'''
+
+import unittest
+from flexaccounassistent.flexAccount import FlexAccount
+from datetime import time
+
+
+
+class FlexAccountTest(unittest.TestCase):
+    def testHourChangeMinus(self):
+        expectedHours = 9
+        expectedMinutes = 0
+        flexAccount = FlexAccount(time(8, 20))
+        flexAccount.sumTime(0, 40)
+        print "Comparing hours"
+        self.assertEquals(expectedHours, flexAccount.hours)
+        print "Comparing minutes"
+        self.assertEquals(expectedMinutes, flexAccount.minutes)
+    
+    def testHourChangePlus(self):
+        self.makeTest(time(8, 20), 0, 50, 9, 10)
+        
+    def testNoHourChange(self):
+        self.makeTest(time(8, 20), 0, -10, 8, 10)
+    
+    def testNegativeBalance(self):
+        self.makeTest(time(8, 20), -9, -10, 0, -50)
+    
+    
+    
+    def makeTest(self, initialBalance, hoursToSum, minutesToSum, expectedHours, expectedMinutes):
+        flexAccount = FlexAccount(initialBalance)
+        flexAccount.sumTime(hoursToSum, minutesToSum)
+        print "Comparing hours"
+        self.assertEquals(expectedHours, flexAccount.hours)
+        print "Comparing minutes"
+        self.assertEquals(expectedMinutes, flexAccount.minutes)
+    
+        
