354eb431acefbde83547a35cce1c81a778ef0610
antek@cruzares.com <antek@cruzares.com>
2012-01-04 22:41:08 +0100

    Added part of UI

--- flexaccountassistent/flexAccountMenu.py
+++ flexaccountassistent/flexAccountMenu.py
@@ -3,11 +3,13 @@ Created on 29/11/2011
 
 @author: Antek
 '''
+from flexaccountassistent.day import DictionaryDayService
 
 welcomeText = u"Flex Account Assistent"
 makeDecision = u"Make you decision"
 addDay = u"1. Add day"
 viewFlexAccount = u"2. View flex account"
+createNowAccount = u"3."
 exit = u"0. Exit program"
 
 valueErrorMsg = u"Wrong value"
@@ -21,14 +23,23 @@ def printMenu():
     print exit
 
 def printValueErrorMsg():
+    
     print valueErrorMsg
-
+    
+EXIT = 0
+VIEW_BALANCE = 2
+NEW_ACCOUNT = 3
+service = None
 if __name__ == "__main__":
     choice = 100
-    while choice != 0:
+    service = DictionaryDayService()
+    while choice != EXIT:
         printMenu()
         try:
             choice = int(raw_input("Your choice: "))
+            if choice == VIEW_BALANCE:
+                print service.getBalance()
+                 
         except EOFError:
             break
         except ValueError:
