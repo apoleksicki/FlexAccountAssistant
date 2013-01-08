34a8bab7078c7b70912ddd79e4dd0b802734801e
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2013-01-08 22:02:40 +0100

    Finished the console ui.

--- flexaccountassistant/commandLineInterface.py
+++ flexaccountassistant/commandLineInterface.py
@@ -13,64 +13,39 @@ def init(args):
 
 def status(args):
     print faa.status()
-
+    
+def add(args):
+    faa.add(faa.createTimeCalculation(args.time))
+    
+def sub(args):
+    timeCalc = faa.createTimeCalculation(args.time)
+    timeCalc = faa.timeCalculationsWithDifferentSign(timeCalc)
+    faa.add(timeCalc)
+    
 def _createParser():
-    parser = argparse.ArgumentParser(description='Helps to follow changes on you flex account.')
-#    parser.add_argument('operation', choices=CHOICES, nargs=2, metavar=('bar', 'baz'))
-#    parser.add_argument('--time')
-    parser.add_argument('--foo', action='store_true', help='foo help')
-    subparsers = parser.add_subparsers(help='sub-command help')
-    initParser = subparsers.add_parser('init', help='inits faa')
+    parser = argparse.ArgumentParser(description='Helps to follow changes on your flex account.')
+    subparsers = parser.add_subparsers(help='Possible options:')
+    initParser = subparsers.add_parser('init', help='Initialises Flex Account Assistant')
     initParser.set_defaults(func=init)
     
-    statusParser = subparsers.add_parser('status', help='Shows the status')
+    statusParser = subparsers.add_parser('status', help='Shows the status.')
     statusParser.set_defaults(func=status)
     
+    addParser = subparsers.add_parser('add', help='Adds a value to your status.')
+    addParser.set_defaults(func=add)
+    addParser.add_argument('time', help='Time you want to add in hh:mm format.')
     
-    parser_a = subparsers.add_parser('a', help='a help')
-    parser_a.add_argument('bar', type=int, help='bar help')
-    
-    parser_b = subparsers.add_parser('b', help='b help')
-    parser_b.add_argument('--baz', choices='XYZ', help='baz help')
-    
+    subParser = subparsers.add_parser('sub',  help='Subtracts a value to your status.')
+    subParser.set_defaults(func=sub)
+    subParser.add_argument('time', help='Time you want to subtract in hh:mm format.')
+       
     return parser
-
-def getArguments():
-    return 'add 2'.split()
-
-#foo = _createParser()
-#foo.parse_args('add 2:30'.split())
 
 if __name__ == '__main__':
     logging.basicConfig(filename=os.path.join(faa.getDefaultPath(), 'faa.log'),format='%(asctime)s %(message)s', level=logging.DEBUG)
-
-    
-
     args = _createParser().parse_args()
     args.func(args)
-#    if args. != None:
-#        print 'foo bar baz'
-#    operation = args.operation
-#    time = args.time
-#    
-#    if operation == CHOICES[0]:
-#        timeCalc = None
-#        if (time != None):
-#            timeCalc = faa.createTimeCalculation(time)
-#        faa.init(initial=timeCalc)
-#    elif operation == CHOICES[1]:
-#        try:
-#            print faa.status()
-#        except IOError:
-#            print "The flex account assistent hasn't been initialised. Please run init." 
-#    elif operation == CHOICES[2] or operation == CHOICES[3]:
-#        timeCalc = faa.createTimeCalculation(time)
-#        if operation == CHOICES[3]:
-#            timeCalc = faa.timeCalculationsWithDifferentSign(timeCalc)
-#        faa.add(timeCalc)
-#    elif operation == CHOICES[4]:
-#        timeCalc = faa.createTimeCalculation(time)
-#        faa.init(timeCalc)
+
    
    
         
